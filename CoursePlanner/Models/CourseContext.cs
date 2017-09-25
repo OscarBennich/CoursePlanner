19178abaa16b7fe734ba54b72b671455aa827a23
USER\osbe1092 <Oscar.Bennich@gmail.com>
2017-09-25 16:58:03 +0200

    Created a persistent database for courses as well as a generated front-end view for this.

---
+++ CoursePlanner/Models/CourseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CoursePlanner.Models
+{
+    
+        public class CourseContext : DbContext
+        {
+            public CourseContext()
+                : base("DefaultConnection")
+            {
+            }
+
+            public DbSet<CourseModels> Courses { get; set; }
+        }
+    
+}
---
+++ CoursePlanner/Models/CourseModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CoursePlanner.Models
+{
+    
+
+    [Table("Course")]
+    public class CourseModels
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string CourseCode { get; set; }
+        public string CourseName { get; set; }
+        public string CourseClassificiation { get; set; }
+        public string CourseType { get; set; }
+        public string Credits { get; set; }
+        public string HSTValue { get; set; }
+        public string BudgetedNumberOfStudents { get; set; }
+        public string CourseBudget { get; set; }
+        public string Term { get; set; }
+        public string Period { get; set; }
+        public string StartDate { get; set; } //shouldn't be a string TESTING
+        public string EndDate { get; set; }
+    }
+}
