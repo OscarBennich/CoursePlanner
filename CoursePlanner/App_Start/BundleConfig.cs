ac92828e18b5e462b3c206f6d4a7df081553824a
USER\rari2223 <Raya.Rizk.2223@student.uu.se>
2017-09-26 15:03:44 +0200

    New template applied to all pages except login

--- CoursePlanner/App_Start/BundleConfig.cs
+++ CoursePlanner/App_Start/BundleConfig.cs
@@ -39,6 +39,26 @@ namespace CoursePlanner
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
 
+            bundles.Add(new StyleBundle("~/Css").Include(
+                      "~/Content/bootstrap/css/bootstrap.min.css",
+                      "~/Content/dist/css/AdminLTE.min.css",
+                      "~/Content/dist/css/skins/skin-red-light.min.css",
+                      "~/Content/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"));
+
+            bundles.Add(new ScriptBundle("~/Js").Include(
+                     "~/Content/plugins/jQuery/jquery-2.2.3.min.js",
+                     "~/Content/plugins/jQueryUI/jquery-ui.min.js",
+                     "~/Content/bootstrap/js/bootstrap.min.js",
+                     "~/Content/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js",
+                     "~/Content/plugins/slimScroll/jquery.slimscroll.min.js",
+                     "~/Content/plugins/fastclick/fastclick.js",
+                     "~/Content/dist/js/app.min.js"
+                     ));
+
+
+
+            BundleTable.EnableOptimizations = true;
+
         }
     }
 }
