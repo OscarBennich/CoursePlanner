1054c9d9fe49940d4b73744a540a35f0ea9b1566
USER\osbe1092 <Oscar.Bennich@gmail.com>
2017-10-08 18:42:22 +0200

    Finished the logic of how allocated, available, and remaining hours are calculated in the teacher profile. The front end still needs some work.

--- CoursePlanner/Controllers/TeacherController.cs
+++ CoursePlanner/Controllers/TeacherController.cs
@@ -35,12 +35,18 @@ namespace CoursePlanner.Controllers
             int baseAnnualWorkingHours = GetBaseAnnualHours(teacher.TeacherDateOfBirth);
             ViewBag.BaseAnnualWorkingHours = baseAnnualWorkingHours;
 
+            ViewBag.TeachingHoursAvailableFall = CalculateTeachingHoursAvailable(teacher, baseAnnualWorkingHours, Terms.Fall);
+            ViewBag.TeachingHoursAvailableSpring = CalculateTeachingHoursAvailable(teacher, baseAnnualWorkingHours, Terms.Spring);
+
+            ViewBag.TeachingHoursAllocatedFall = CalculateTeachingHoursAllocated(teacher, Terms.Fall);
+            ViewBag.TeachingHoursAllocatedSpring = CalculateTeachingHoursAllocated(teacher, Terms.Spring);
+
             List<CourseOccurrence> courseOccurencesFall = GetTeacherCourses(teacher.TeacherId, Terms.Fall).ToList();
             List<CourseOccurrence> courseOccurencesSpring = GetTeacherCourses(teacher.TeacherId, Terms.Spring).ToList();
 
             ViewBag.CourseOccurencesFall = courseOccurencesFall;
             ViewBag.CourseOccurencesSpring = courseOccurencesSpring;
-
+         
             return View(teacher);
         }
 
@@ -166,28 +172,72 @@ namespace CoursePlanner.Controllers
             }
         }
 
-        private int CalculateTeachingHoursAvailable(Teacher teacher, int baseAnnualWorkingHours, Terms term)
+        private double CalculateTeachingHoursAvailable(Teacher teacher, int baseAnnualWorkingHours, Terms term) //should split into terms, testing for now
+        {   
+            int teachingHoursAvailable = 0;
+            double totalReductionPercentage = 1;
+
+            if(term.Equals(Terms.Fall)) 
+            {   
+                // First split the base amount in 2 (2 terms in one year)
+                // Then multiply by the total percentage for that term (for example *0,5)
+                // This gives us the base available hours for this teacher for this term
+                teachingHoursAvailable = Convert.ToInt32(baseAnnualWorkingHours/2 * teacher.TotalPercentageFall);
+
+                try // Error handling for the case that the teacher has no reductions
+                {
+                    // Once we have the base amount we apply the sum of all other reductions (Research, Assignments, Administration and Other reductions) for that term
+                    // For example a professor (per default) would have 50% research and 10% administration so the sum is 0,5 + 0,1 = 0,6
+                    totalReductionPercentage = (double)db.TeacherReduction.Where(x => x.TeacherId == teacher.TeacherId && x.Term == Terms.Fall).Select(y => y.Percentage).Sum();
+                }
+                catch { }
+            }
+            else 
+            {   
+                teachingHoursAvailable = Convert.ToInt32(baseAnnualWorkingHours/2 * teacher.TotalPercentageSpring);
+
+                try 
+                {
+                    totalReductionPercentage = (double)db.TeacherReduction.Where(x => x.TeacherId == teacher.TeacherId && x.Term == Terms.Spring).Select(y => y.Percentage).Sum();
+                }
+                catch { }
+            }
+
+            // Finally we apply the total reduction to the base value for the term
+            // For example a base of 200 hours for this term for a professor would result in 
+            // 200 * 0,6 = 120 hours
+            return teachingHoursAvailable = Convert.ToInt32(teachingHoursAvailable * totalReductionPercentage);
+        }
+
+        private int CalculateTeachingHoursAllocated(Teacher teacher, Terms term)
         {
-            int teachingHoursAvailable = baseAnnualWorkingHours;
+            int teachingHoursAllocated = 0;
 
-            if (term.Equals(Terms.Fall))
+            if (term.Equals(Terms.Fall)) 
             {
-                //teacher.
-                //teachingHoursAvailable 
+                try
+                {   
+                    // Calculate the sum of all hours allocated for this teacher in all course occurances which match
+                    // with the teacherID and where the term matches (Fall in this case)
+                    teachingHoursAllocated += Convert.ToInt32(db.CourseTeacher.Where(x => x.TeacherId == teacher.TeacherId && x.CourseOccurrence.Term == Terms.Fall).Select(y => y.Hours).Sum());
+                }
+                catch { }
             }
-            else
+            else 
             {
-
+                try
+                {
+                    teachingHoursAllocated += Convert.ToInt32(db.CourseTeacher.Where(x => x.TeacherId == teacher.TeacherId && x.CourseOccurrence.Term == Terms.Spring).Select(y => y.Hours).Sum());
+                }
+                catch { }
             }
 
-            //if(teacher.TeacherPosition.Equals(Positions.JuniorLecturer)
-            return 0;
+            return teachingHoursAllocated;
         }
 
         private IEnumerable<CourseOccurrence> GetTeacherCourses(int teacherID, Terms term)
         {
             return db.CourseTeacher.Where(c => c.TeacherId == teacherID && c.CourseOccurrence.Term == term).Select(c => c.CourseOccurrence);
-            //return db.CourseOccurrence.Where(c => c.CourseTeacher.Where(t => t.TeacherId == teacherID).FirstOrDefault && c.Term == term);
         }
     }
 }
