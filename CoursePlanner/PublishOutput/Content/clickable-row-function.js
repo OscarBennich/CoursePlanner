a7f34a419868d1ed979412ed8d32522a7707952a
USER\rari2223 <Raya.Rizk.2223@student.uu.se>
2017-10-10 15:41:58 +0200

    Deleting a bunch of files and new publish profile

--- CoursePlanner/PublishOutput/Content/clickable-row-function.js
+++
@@ -1,9 +0,0 @@
-﻿
- $(document).ready(function () {
-     $(".clickable-row").click(function () {
-         //var url = '@Url.Action("Details", "Course", new { id = "__id__" })';
-         window.location.href = $(this).data("href");
-         //window.location = 'Details/' + $(this).data("href");
-     });
-
- });
